0ba25ef34aa68127445746656711217e29801811
jqkk <kkj2551@naver.com>
2023-09-06 09:58:01 +0900

    feat: Textarea 컴포넌트 구현 완료

---
+++ packages/design-system/src/components/textarea/Textarea.css.ts
@@ -0,0 +1,15 @@
+import { style } from "@vanilla-extract/css";
+
+export const textareaStyle = style({
+  display: 'block',
+  padding: "8px 12px",
+  fontSize: '16px',
+  lineHeight: 1.25,
+  color: '#55595c',
+  backgroundColor: '#fff',
+  border: '1px solid rgba(0, 0, 0, 0.15)',
+  borderRadius: '4px',
+  resize: 'vertical',
+  width: '100%',
+  boxSizing: 'border-box',
+})
---
+++ packages/design-system/src/components/textarea/Textarea.stories.tsx
@@ -0,0 +1,14 @@
+import type { Meta } from "@storybook/react";
+import { Textarea } from "./Textarea";
+
+const meta: Meta<typeof Textarea> = {
+  title: "Components/Textarea",
+  component: Textarea,
+  tags: ["autodocs"],
+}
+
+export default meta;
+
+export const Default = () => {
+  return <Textarea style={{ height: '351px'}}/>
+}
