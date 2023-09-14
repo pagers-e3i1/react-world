6bdd56d574c9c520f0db528166c7f864e681192e
jqkk <kkj2551@naver.com>
2023-09-14 20:28:58 +0900

    modify: 린트 적용 후 수정

--- packages/design-system/src/components/textarea/Textarea.stories.tsx
+++ packages/design-system/src/components/textarea/Textarea.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta } from "@storybook/react";
+
 import { Textarea } from "./Textarea";
 
 const meta: Meta<typeof Textarea> = {
--- packages/design-system/vite.config.ts
+++ packages/design-system/vite.config.ts
@@ -2,7 +2,6 @@ import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 
 //TODO : build설정 수정 필요
 export default defineConfig({
