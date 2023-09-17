3eb0c6400288114d1768ccbfdc7a50f20aaff11d
jqkk <kkj2551@naver.com>
2023-09-17 19:16:05 +0900

    bugfix: TextInput 타입 수정

--- packages/design-system/src/components/text-input/TextInput.tsx
+++ packages/design-system/src/components/text-input/TextInput.tsx
@@ -1,6 +1,8 @@
 import { input } from "./style.css";
 
-interface Props extends React.HTMLAttributes<HTMLInputElement>, StyleProps {}
+interface Props
+  extends React.InputHTMLAttributes<HTMLInputElement>,
+    StyleProps {}
 
 export default function TextInput(props: Props) {
   return <input {...props} className={`${input} ${props.className}`} />;
