c89ce7538734bd3eb90f3f48385bd0e5ccb88e03
jqkk <kkj2551@naver.com>
2023-09-17 19:17:42 +0900

    modify: app에서 design-system 가져오기

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,38 +1,29 @@
-import './App.css'
-
-import { useState } from 'react'
-
-import viteLogo from '/vite.svg'
-
-import reactLogo from './assets/react.svg'
+import { Divider, Skeleton, Textarea, TextInput } from "design-system";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [inputValue, setInputValue] = useState("");
+
+  const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div
+      style={{
+        padding: "20px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "16px",
+      }}
+    >
+      <h1>리얼월드 START!!</h1>
+      <Divider />
+      <Skeleton borderRadius="90px" width="90px" height="90px" />
+      <TextInput onChange={handleChangeInput} value={inputValue} />
+      <Textarea />
+    </div>
+  );
 }
 
-export default App
+export default App;
