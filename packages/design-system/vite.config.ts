d91919d18c7d93a64667524976e88e10ac4fa0cf
jqkk <kkj2551@naver.com>
2023-09-17 19:15:34 +0900

    setting: 디자인 시스템 빌드 설정

--- packages/design-system/vite.config.ts
+++ packages/design-system/vite.config.ts
@@ -1,18 +1,23 @@
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 //TODO : build설정 수정 필요
 export default defineConfig({
   base: "./",
-  plugins: [react(), tsconfigPaths(), vanillaExtractPlugin()],
+  plugins: [react(), tsconfigPaths(), vanillaExtractPlugin(), dts()],
   build: {
     lib: {
-      entry: "src/main.tsx",
-      name: "main",
+      entry: "src/index.ts",
       formats: ["es", "cjs"],
       fileName: "index",
+    },
+    rollupOptions: {
+      output: {
+        intro: 'import "./style.css"',
+      },
     },
   },
 });
