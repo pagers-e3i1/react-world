e178b65b925b77b4fffa00a1578c7248917536bc
동동이 <drdragon777@naver.com>
2023-09-21 02:34:42 +0900

    setting: vanilla extract vite plugin

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -1,5 +1,6 @@
 /// <reference types="vitest" />
 
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
@@ -7,7 +8,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), vanillaExtractPlugin()],
   server: {
     port: 3000,
   },
