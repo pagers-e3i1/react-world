3f38d8790bf4f7af0302401e1118243cf3dc1e0c
jqkk <kkj2551@naver.com>
2023-09-20 22:03:47 +0900

    fix: textarea props 수정

--- packages/design-system/src/components/textarea/Textarea.tsx
+++ packages/design-system/src/components/textarea/Textarea.tsx
@@ -1,6 +1,9 @@
-import { textareaStyle } from "./Textarea.css"
+import { textareaStyle } from "./Textarea.css";
 
 /** 기본적인 Textarea 컴포넌트입니다. */
-export const Textarea = ({ className, ...rest}: React.HTMLAttributes<HTMLTextAreaElement>) => {
-  return <textarea className={`${textareaStyle} ${className}`} {...rest} />
-}
+export const Textarea = ({
+  className,
+  ...rest
+}: React.TextareaHTMLAttributes<HTMLTextAreaElement>) => {
+  return <textarea className={`${textareaStyle} ${className}`} {...rest} />;
+};
