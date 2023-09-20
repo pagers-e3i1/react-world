33b0e70ecee245425a4e04495b9070660b8ff1ca
동동이 <drdragon777@naver.com>
2023-09-21 02:28:57 +0900

    setting: test env setup

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -1,11 +1,19 @@
+/// <reference types="vitest" />
+
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
   plugins: [react(), tsconfigPaths()],
   server: {
     port: 3000,
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./vitest.setup.ts",
   },
 });
