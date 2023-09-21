f5e14ea54b9133f990de25d4e4f33361db483016
2dowon <dwon424@gmail.com>
2023-09-21 21:41:25 +0900

    setting: vanilla extract vite plugin 추가

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -1,10 +1,11 @@
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   base: "./",
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), vanillaExtractPlugin()],
   server: {
     port: 3000,
   },
