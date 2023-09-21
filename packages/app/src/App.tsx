d24063b597a0c0aff3ffdf82332bbc8350d21e26
seulgi <sgsg9447@gmail.com>
2023-09-21 21:22:24 +0900

    modify: Button, Chip, Text export 수정

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,4 +1,11 @@
-import { Divider, Skeleton, Textarea, TextInput } from "design-system";
+import {
+  Button,
+  Divider,
+  Skeleton,
+  Text,
+  Textarea,
+  TextInput,
+} from "design-system";
 import { useState } from "react";
 
 function App() {
@@ -9,20 +16,25 @@ function App() {
   };
 
   return (
-    <div
-      style={{
-        padding: "20px",
-        display: "flex",
-        flexDirection: "column",
-        gap: "16px",
-      }}
-    >
-      <h1>리얼월드 START!!</h1>
-      <Divider />
-      <Skeleton borderRadius="90px" width="90px" height="90px" />
-      <TextInput onChange={handleChangeInput} value={inputValue} />
-      <Textarea />
-    </div>
+    <>
+      <div
+        style={{
+          padding: "20px",
+          display: "flex",
+          flexDirection: "column",
+          gap: "16px",
+        }}
+      >
+        <h1>리얼월드 START!!</h1>
+        <Divider />
+        <Skeleton borderRadius="90px" width="90px" height="90px" />
+        <TextInput onChange={handleChangeInput} value={inputValue} />
+        <Textarea />
+        <Button>Button</Button>
+        <Text>Typography</Text>
+        <Text typography="h1">h1</Text>
+      </div>
+    </>
   );
 }
 
